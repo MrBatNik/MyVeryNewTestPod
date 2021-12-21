031e14aff71aed7c99044dd9ddd8f4d34c3b5146
Nikita <nicandreo@gmail.com>
2021-12-21 17:39:29 +0300

    New Try

---
+++ MyPodFile.swift
@@ -0,0 +1,18 @@
+//
+//  MyPodFile.swift
+//  MyVeryNewTestPod
+//
+//  Created by Никита Бат on 21.12.2021.
+//
+
+import Foundation
+
+struct SayHi {
+    
+    public init() {}
+    
+    public func sayHi() {
+        print("Hello World")
+    }
+    
+}
